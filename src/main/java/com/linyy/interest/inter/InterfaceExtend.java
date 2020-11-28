6f33650482121873b5e75a7d46a503f356988e89
gitadmini <457938831@qq.com>
2020-11-28 20:15:36 +0800

    继承、接口

---
+++ src/main/java/com/linyy/interest/inter/InterfaceExtend.java
@@ -0,0 +1,32 @@
+package com.linyy.interest.inter;
+
+/**
+ * 接口、继承 测试
+ */
+public class InterfaceExtend extends Ex implements Inter{
+
+    public static void main(String[] args) {
+
+        // 验证了接口和继承都可以直接new一个子类
+        Inter inter = new InterfaceExtend();
+        inter.interFunc();
+
+        Ex ex = new InterfaceExtend();
+        ex.exFunc();
+    }
+}
+
+interface Inter{
+
+    default void interFunc(){
+        System.out.println("inter func...");
+    };
+
+}
+
+class Ex{
+
+    void exFunc(){
+        System.out.println("ex func...");
+    }
+}
