2d3271802035884b2420470eb9bc248b4340dc0b
gitadmini <457938831@qq.com>
2020-12-20 19:34:20 +0800

    chapter1

---
+++ src/main/java/com/linyy/interest/neural/NeuralNetTest.java
@@ -0,0 +1,9 @@
+package com.linyy.interest.neural;
+
+public class NeuralNetTest {
+	public static void main(String[] args) {
+		NeuralNet n = new NeuralNet();
+		n.initNet();
+		n.printNet();
+	}
+}
